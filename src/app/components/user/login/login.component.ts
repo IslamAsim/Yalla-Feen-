87eb5a419ced1f4c6bb668be0f118f5b73a59c63
salsabeelgamal <salsabeelgamal6@gmail.com>
2021-01-26 01:56:03 +0200

    login component edit - Salsabeel

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      Email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+        ],
+      ],
+      Password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(18),
+        ],
+      ],
+    });
   }
-
 }
--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -5,21 +5,33 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { HostProfileComponent } from './host-profile/host-profile.component';
-import {RouterModule, Routes} from '@angular/router';
-import {NgSelectModule} from '@ng-select/ng-select';
-import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'sign-up', component: SignUpComponent},
-  {path: 'reset-password', component: ResetPasswordComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'host-profile', component: HostProfileComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'host-profile', component: HostProfileComponent },
 ];
 
 @NgModule({
-  declarations: [LoginComponent, SignUpComponent, ProfileComponent, ResetPasswordComponent, HostProfileComponent],
+  declarations: [
+    LoginComponent,
+    SignUpComponent,
+    ProfileComponent,
+    ResetPasswordComponent,
+    HostProfileComponent,
+  ],
   imports: [
-    CommonModule, RouterModule.forChild(routes), NgSelectModule, NgbCarouselModule,
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    NgSelectModule,
+    NgbCarouselModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
-export class UserModule { }
+export class UserModule {}
